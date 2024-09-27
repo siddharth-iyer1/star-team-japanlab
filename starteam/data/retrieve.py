b23f469fa81b83de6f0d03866de0323198c286a7
Siddharth <siddharth.iyer123@gmail.com>
2024-09-27 14:44:28 -0500

    secret removed

--- starteam/data/retrieve.py
+++ starteam/data/retrieve.py
@@ -5,8 +5,8 @@ import os
 app = FastAPI()
 
 # Initialize Supabase client
-supabase_url = 'https://bixjwbynjtwzcogjznia.supabase.co'
-supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpeGp3YnluanR3emNvZ2p6bmlhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjc3MDQwMywiZXhwIjoyMDQyMzQ2NDAzfQ.bttc6NPT8-SI2oxKHlmf576jXaTfin7kSj1emPpiHJI'
+supabase_url = 'url'
+supabase_key = 'key'
 supabase_client = supabase.create_client(supabase_url, supabase_key)
 
 @app.get("/movies/era/{era_id}")
